63db66c3945a07c7e6930a6d9cce2b46e2760df3
Rick03 <hsoorang@naver.com>
2026-02-16 00:04:39 +0900

    fix: revert theme and export config to defaults for web deployment
    - Remove subset font from theme (causes compatibility issues)
    - Remove CSS style from html/head_include (not working)
    - Revert to clean default config
    - Korean text converted to English (commit d5beac7)
    - Focus on stable web deployment

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771164363|1642125';
+const CACHE_VERSION = '1771167875|1690873';
 /** @type {string} */
 const CACHE_PREFIX = 'Blacksmith-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
