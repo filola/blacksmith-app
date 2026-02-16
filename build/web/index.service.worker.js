39460687102084f1e787ab6f55908d400f88f22d
Rick03 <hsoorang@naver.com>
2026-02-16 12:38:45 +0900

    build: web export v0.5.0-feature (all tabs visible, English text)

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771212474|2234171';
+const CACHE_VERSION = '1771213113|2161370';
 /** @type {string} */
 const CACHE_PREFIX = 'Blacksmith-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
