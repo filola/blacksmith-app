1af253093d68071ae5d6f66bf1431ea39edc9302
filola <filola@users.noreply.github.com>
2026-02-17 02:34:13 +0000

    deploy: 07cc8288d78c4c2cadf587aaea909d43186bf328

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771293426|6664645';
+const CACHE_VERSION = '1771295651|4525792';
 /** @type {string} */
 const CACHE_PREFIX = 'Blacksmith-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
